242bd598dd79f217372519447c9ed2b3cadd5f05
Jonas Martinsson <jonas@thinktrain.se>
2016-05-13 00:13:31 +0200

    mod5 2

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -96,14 +96,13 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtmlUrl) {
+    function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      console.log(chosenCategoryShortName.short_name);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -119,10 +118,9 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = 
         
-        insertProperty( homeHtmlUrl, 
+        insertProperty( homeHtml, 
                         "randomCategoryShortName",
                         "'" + chosenCategoryShortName.short_name + "'");
-        console.log(homeHtmlToInsertIntoMainPage)
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
